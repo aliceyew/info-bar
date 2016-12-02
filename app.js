7ce35b9187057eae5a1242fb2b29534519427529
ayew <yewaili@gmail.com>
2016-12-02 09:15:42 -0500

    Added connect.html to test

--- app.js
+++ app.js
@@ -10,19 +10,28 @@ var express = require('express');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
-var cfenv = require('cfenv');
+//var cfenv = require('cfenv');
 
 // create a new express server
+http = require('http');
 var app = express();
+var server = http.createServer(app);
+
+var io = require('socket.io').listen(server);
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
 
+//var io = require("socket.io")(http);
 // get the app environment from Cloud Foundry
-var appEnv = cfenv.getAppEnv();
-
+//var appEnv = cfenv.getAppEnv();
+var host = (process.env.VCAP_APP_HOST || 'localhost');
+var port = (process.env.CVAP_APP_PORT || 3001);
 // start server on the specified port and binding host
-app.listen(appEnv.port, '0.0.0.0', function() {
-  // print a message when the server starts listening
-  console.log("server starting on " + appEnv.url);
+var server = app.listen(port, host, function() {
+	console.log("Server starting on" + host + ":" + port);
 });
+//app.listen(appEnv.port, '0.0.0.0', function() {
+  // print a message when the server starts listening
+//  console.log("server starting on " + appEnv.url);
+//});

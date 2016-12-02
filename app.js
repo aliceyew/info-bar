4f5484ef299d963be5865de72a8ede8d210ebe6f
ayew <yewaili@gmail.com>
2016-12-02 11:41:16 -0500

    Testing connect

--- app.js
+++ app.js
@@ -6,18 +6,27 @@
 
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
+
+// Must use HTTPS for chrome and firefox
+var isUseHTTPs = true;
+
 var express = require('express');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 //var cfenv = require('cfenv');
+var fs = require('fs');
+var options = {
+	key: fs.readFileSync('fake-keys/privatekey.pem'),
+	cert: fs.readFileSync('fake-keys/certificate.pem')
+};
 
 // create a new express server
-http = require('http');
+http = require("https");
 var app = express();
-var server = http.createServer(app);
+var server = http.createServer(options, app);
 
-var io = require('socket.io').listen(server);
+//var io = require('socket.io').listen(server);
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
@@ -31,6 +40,8 @@ var port = (process.env.CVAP_APP_PORT || 3001);
 var server = app.listen(port, host, function() {
 	console.log("Server starting on" + host + ":" + port);
 });
+
+require('./Signaling-Server.js')(server);
 //app.listen(appEnv.port, '0.0.0.0', function() {
   // print a message when the server starts listening
 //  console.log("server starting on " + appEnv.url);
